b4a5ec6667dd3f32b150342195af357c0efbd246
Darlan Araujo <darlanparaujo@outlook.com>
2022-07-22 15:47:26 -0300

    Desafio #7 - Atualização 7
    Foi feito novas melhorias do HTML e JS.
    O HTML já recebe os arquivos que vem do JSON. Mas precisa melhorar a logica para habilitar os botões e fazer funcionar todos os recursos.

--- js/script.js
+++ js/script.js
@@ -1,87 +1,113 @@
+// COMANDOS PARA PUXAR O CONTEÚDO DO JSON E ORGANIZAR NA PÁGINA ============================================
 const currentUser = document.querySelectorAll('.currentUser');
 const comments = document.querySelectorAll('.comments');
 const replices = document.querySelectorAll('.replices');
 
-console.log(comments);
-
 let jsonDados = [
     {
         "currentUser": {
-            "image": {
-                "png": "./images/avatars/image-juliusomo.png",
-                "webp": "./images/avatars/image-juliusomo.webp"
-            },
-            "username": "juliusomo"
+          "image": { 
+            "png": "./images/avatars/image-juliusomo.png",
+            "webp": "./images/avatars/image-juliusomo.webp"
+          },
+          "username": "juliusomo"
         },
         "comments": [
-            {
-                "id": 1,
-                "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-                "createdAt": "1 month ago",
-                "score": 12,
-                "user": {
-                    "image": {
-                        "png": "./images/avatars/image-amyrobson.png",
-                        "webp": "./images/avatars/image-amyrobson.webp"
-                    },
-                    "username": "amyrobson"
-                },
-                "replies": []
+          {
+            "id": 1,
+            "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+            "createdAt": "1 month ago",
+            "score": 12,
+            "user": {
+              "image": { 
+                "png": "./images/avatars/image-amyrobson.png",
+                "webp": "./images/avatars/image-amyrobson.webp"
+              },
+              "username": "amyrobson"
             },
-            {
-                "id": 2,
-                "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-                "createdAt": "2 weeks ago",
-                "score": 5,
+            "replies": [
+              {
+                "id": 3,
+                "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+                "createdAt": "1 week ago",
+                "score": 4,
+                "replyingTo": "maxblagun",
                 "user": {
-                    "image": {
-                        "png": "./images/avatars/image-maxblagun.png",
-                        "webp": "./images/avatars/image-maxblagun.webp"
-                    },
-                    "username": "maxblagun"
-                },
-                "replies": [
-                    {
-                        "id": 3,
-                        "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-                        "createdAt": "1 week ago",
-                        "score": 4,
-                        "replyingTo": "maxblagun",
-                        "user": {
-                            "image": {
-                                "png": "./images/avatars/image-ramsesmiron.png",
-                                "webp": "./images/avatars/image-ramsesmiron.webp"
-                            },
-                            "username": "ramsesmiron"
-                        }
-                    },
-                    {
-                        "id": 4,
-                        "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-                        "createdAt": "2 days ago",
-                        "score": 2,
-                        "replyingTo": "ramsesmiron",
-                        "user": {
-                            "image": {
-                                "png": "./images/avatars/image-juliusomo.png",
-                                "webp": "./images/avatars/image-juliusomo.webp"
-                            },
-                            "username": "juliusomo"
-                        }
-                    }
-                ]
-            }
+                  "image": { 
+                    "png": "./images/avatars/image-ramsesmiron.png",
+                    "webp": "./images/avatars/image-ramsesmiron.webp"
+                  },
+                  "username": "ramsesmiron"
+                }
+              },
+              {
+                "id": 4,
+                "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+                "createdAt": "2 days ago",
+                "score": 2,
+                "replyingTo": "ramsesmiron",
+                "user": {
+                  "image": { 
+                    "png": "./images/avatars/image-juliusomo.png",
+                    "webp": "./images/avatars/image-juliusomo.webp"
+                  },
+                  "username": "juliusomo"
+                }
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+            "createdAt": "2 weeks ago",
+            "score": 5,
+            "user": {
+              "image": { 
+                "png": "./images/avatars/image-maxblagun.png",
+                "webp": "./images/avatars/image-maxblagun.webp"
+              },
+              "username": "maxblagun"
+            },
+            "replies": [
+              {
+                "id": 3,
+                "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+                "createdAt": "1 week ago",
+                "score": 4,
+                "replyingTo": "maxblagun",
+                "user": {
+                  "image": { 
+                    "png": "./images/avatars/image-ramsesmiron.png",
+                    "webp": "./images/avatars/image-ramsesmiron.webp"
+                  },
+                  "username": "ramsesmiron"
+                }
+              },
+              {
+                "id": 4,
+                "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+                "createdAt": "2 days ago",
+                "score": 2,
+                "replyingTo": "ramsesmiron",
+                "user": {
+                  "image": { 
+                    "png": "./images/avatars/image-juliusomo.png",
+                    "webp": "./images/avatars/image-juliusomo.webp"
+                  },
+                  "username": "juliusomo"
+                }
+              }
+            ]
+          }
         ]
-    }
+      }
 ];
 let cont = 0;
-let cont2 = 0;
 
 jsonDados.forEach((dados) => {
     
     currentUser.forEach((item) => {
         item.getElementsByClassName('js-image')[0].src = dados.currentUser.image.png;
-        
     });
 
     comments.forEach((item) => {
@@ -89,17 +115,40 @@ jsonDados.forEach((dados) => {
         item.getElementsByClassName('js-username')[0].innerHTML = dados.comments[cont].user.username;
         item.getElementsByClassName('js-createdAt')[0].innerHTML = dados.comments[cont].createdAt;
         item.getElementsByClassName('js-score')[0].innerHTML = dados.comments[cont].score;
+        item.getElementsByClassName('js-content')[0].innerHTML = dados.comments[cont].content;
+        item.setAttribute('id', `comment${cont +1}`);
         cont++;
     });
+    cont = 0;
 
     replices.forEach((item) => {
-        // if(dados.comments[cont2.replices[cont2].leg])
-        // item.getElementsByClassName('js-image')[0].src = dados.comments[cont2].replies[cont2].user.image.png;
-        cont2++;
-        console.log(cont2);
+        item.getElementsByClassName('js-image')[0].src = dados.comments[cont].replies[cont].user.image.png;
+        item.getElementsByClassName('js-username')[0].innerHTML = dados.comments[cont].replies[cont].user.username;
+        item.getElementsByClassName('js-createdAt')[0].innerHTML = dados.comments[cont].replies[cont].createdAt;
+        item.getElementsByClassName('js-score')[0].innerHTML = dados.comments[cont].replies[cont].score;
+        item.getElementsByClassName('js-replyngto')[0].innerHTML = `@${dados.comments[cont].replies[cont].replyingTo}`;
+        item.getElementsByClassName('js-content')[0].innerHTML = dados.comments[cont].replies[cont].content;
+        item.setAttribute('id', `replice${cont +1}`);
+        cont++;
+    });
+    cont = 0;
+});
+// ====================================================================================================================
+
+// CONTROLE DOS BOTÕES ================================================================================================
+
+
+const comment = document.querySelectorAll('[id^="comment"]');
+const replice = document.querySelectorAll('[id^="replice"]');
+
+replice.forEach((dados) => {
+    // console.log(dados);
+    // console.log(dados.getElementsByClassName('btn-mais'));
+    dados.getElementsByClassName('btn-editar')[0].addEventListener('click', (event) => {
+        console.log('Clicou!');
+        // event.currentTarget.style.color = '#000';
     });
 });
-
 
 
 
